7967781432d900eaff933feeb0383e9ab5045513
Merve ÖZ <104096244+themervez@users.noreply.github.com>
2022-12-25 15:45:54 +0300

    Add project files.

---
+++ WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DAL;
+using WebAPI.DAL.Entities;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        Context context = new Context();
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = context.Categories.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CategoryList(Category category)
+        {
+            var values = context.Categories.Add(category);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult CategoryGet(int id)
+        {
+            var values = context.Categories.Find(id);
+            return Ok(values);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult CategoryDelete(int id)
+        {
+            var values = context.Categories.Find(id);
+            context.Categories.Remove(values);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult CategoryUpdate(Category category)
+        {
+            var values = context.Categories.Find(category.ID);
+            values.Name = category.Name;
+            values.Description = category.Description;
+            values.Status = category.Status;
+            context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DAL.Entities;
+
+namespace WebAPI.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=WebAPIDb;Trusted_Connection=True;");
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
